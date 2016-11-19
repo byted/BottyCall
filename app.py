41b684739435d05e4848da5ff3913656dc5c92bf
Stefan Selent <sselent@babbel.com>
2016-11-19 13:09:23 +0100

    n

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def verify():
 def webhook():
 
     # endpoint for processing incoming messaging events
-    log("---------------huhu")
+    print("---------------huhu")
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
