88b322de89eb3f4f9c2af37846ce1139fc6bf2da
Stefan Selent <sselent@babbel.com>
2016-11-20 11:07:10 +0100

    foobar

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import requests
 from flask import Flask, request, jsonify
 
 # scope = 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private streaming user-library-read'
-token = 'BQAyN5tuFrseBW657giCP0RsJ9DQnut49bQgT0v_e0wJ6CzhtTt8g5CAaHo-apiQAB3JYkGyR94zNRN8HOzAuZTmPJv3A0tNuISErumFLgNFlKm9blsH4FY7t71YSvKaHoFywmh59gsFQD1OxyZxETJhGQ2PF2atprX_wqr6IRoaerg9qPzjM6OHvS6wa25OODZfGYbC2yCg4-yhgGSQsknBLM4SQtORXCB5Ir1M8cszFqMhFEKE7YenVhlizuWqaooTVJTb0soOkajn8XEDembaD2HX6Uj2XfeMhBBN_lJmpJoo'
+
+token='BQBQdYv3_ZoWzieGgiLg-tYv2weboZ6WJW1i_U1TFHoBmWajx9c1HCSktBAxodEjGuNnLSACP54re3IGjPl9SdiBi0ANKfoLvKJGk8jK8pXGovEEXvUsju9hvcQkDVFhwus5ApEPCFIm74f-z1SmeKCk87eKmWHJvc91OdS5KcLRZkV4xegW5wnrhacLQw4Wh4Y_PurFqepaHlJ3xmphwEXNphBcdqwbt07S7HAp6-Yl1nAjfQalfqTdoJK-DFkwtblO8SVLKstSFzkEnB5-luiNxDbtmBMO_JPhvGNzxHpEXbLr'
 app = Flask(__name__)
 
 
@@ -65,13 +66,70 @@ def webhook():
             'displayText': 'foobar',
             'data': {'facebook': fb_msg, 'slack': slack_msg }
         }
+    elif data['result']['metadata']['intentName'] in ['ON-6-Change Music']:
+        artist = search_artist(data['result']['parameters']['artist'])
+        msg1 = [
+            'Of course, Babe, that\'s no Problem.',
+            'Sure, Babe.',
+            'I am sorry you didn\'t like what I have shown you...'
+        ]
+        msg2 = [
+            'I think {} is what you\'re looking for.',
+            '{} should do the trick.',
+            'Here is something nice by {}.'
+        ]
+        if artist is not None:
+            fb_msg = {
+                # 'text': 'How about something by {}?\n {}'.format(song['artist'], song['url']) 
+                "attachment": {
+                    "type":"template",
+                    "payload": {
+                        "template_type":"generic",
+                        "elements":[
+                            {
+                                "title": artist['artist'],
+                                "item_url": artist['url'],
+                                "image_url": artist['img_url']
+                            }
+                        ]
+                    }
+                }
+            }
+            slack_msg = { 'text': '{}\n{}\n{}'.format(random.choice(msg1), random.choice(msg2).format(artist['artist']), artist['url']) }
+            res = {
+                'displayText': 'foobar',
+                'data': {'facebook': fb_msg, 'slack': slack_msg }
+            }
+            if data['originalRequest']['source'] == 'facebook':
+                sender_id = data['originalRequest']['data']['sender']['id']
+                send_fb_message(sender_id, random.choice(msg1))
+                send_fb_message(sender_id, random.choice(msg2).format(artist['artist']))
+        else:
+            res = {
+                'displayText': 'Sorry, I have nothing for {}'.format(artist['artist']),
+                # 'data': {'facebook': fb_msg, 'slack': slack_msg }
+            }
 
     return jsonify(res)
 
 
 @app.route('/init', methods=['GET'])
 def init():
-    return send_fb_message('1130764070353241', 'buh')
+    uid = request.args.get('uid')
+    first = [
+        'Sweetheart!',
+        'Hey babe.',
+        'Hi sweetie!'
+    ]
+    second = [
+        'I was just thinking of you... in a special way ;) Are you in the mood for some play-time?',
+        'Are you in the mood for some one-on-one time with me?',
+        'I thought you might be in the mood for some special attention today...'
+    ]
+    res = []
+    res.append(send_fb_message(uid, random.choice(first)))
+    res.append(send_fb_message(uid, random.choice(second)))
+    return '<br>'.join(res)
 
 
 @app.route('/rec', methods=['GET'])
@@ -79,7 +137,17 @@ def rec():
     return jsonify(get_rec())
 
 
-
+def search_artist(artist):
+    result = sp.search(q=artist, type='artist')
+    if len(result['artists']['items']) > 0:
+        artist = result['artists']['items'][0]
+        return {
+            'artist': artist['name'],
+            'url': artist['external_urls']['spotify'],
+            'img_url': artist['images'][1]['url']
+        }
+    else:
+        return None
 
 
 def get_rec():
@@ -121,7 +189,7 @@ def send_fb_message(recipient_id, message_text):
         log(r.text)
         return r.text, r.status
     else:
-        return 'sent message {} to user_id {}'.format(message_text, recipient_id), 200
+        return 'sent message "{}" to user_id {}'.format(message_text, recipient_id)
 
 
 def log(message):  # simple wrapper for logging to stdout on heroku
